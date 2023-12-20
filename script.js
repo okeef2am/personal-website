02387fcf7a9e4706df32594686bc6805af895402
Allie O'Keeffe <okeef2am@dukes.jmu.edu>
2023-12-19 21:08:24 -0500

    Added typing feature and change skills section

---
+++ script.js
@@ -0,0 +1,6 @@
+let typed = new Typed(".auto-type", {
+    strings: ["a student.", "a James Madison Duke.", "a Computer Scientist.", "adventurous.", "driven."],
+    typeSpeed: 150,
+    backSpeed: 125,
+    looptrue: true
+});
