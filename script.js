93667347a80afeee6b40a60d8c593ab05afa16d7
Allie O'Keeffe <okeef2am@dukes.jmu.edu>
2023-12-19 21:35:47 -0500

    Added items to skill section

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let typed = new Typed(".auto-type", {
     strings: ["a student.", "a James Madison Duke.", "a Computer Scientist.", "adventurous.", "driven."],
     typeSpeed: 150,
-    backSpeed: 125,
-    looptrue: true
+    backSpeed: 100,
+    looptrue: true,
 });
